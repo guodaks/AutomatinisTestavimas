e645d63639386b2754ad044e95986df009470955
Guoda Slipkute <guoda.slipkute@gmail.com>
2021-11-19 20:06:22 +0200

    5th commit

---
+++ automatinis_testavimas/baig page/ParduotuvePegasasLogInPage.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatinis_testavimas.page
+{
+    public class ParduotuvePegasasLogInPage : BasePage
+    {
+        private const string PageAddress = "https://www.pegasas.lt/customer/account/login/referer/aHR0cHM6Ly93d3cucGVnYXNhcy5sdC8=";
+        //private const string ResultText = "";
+        private IWebElement _emailInputField => Driver.FindElement(By.Id("email"));
+        private IWebElement _passwordInputField => Driver.FindElement(By.Id("pass"));
+        private IWebElement _loginButton => Driver.FindElement(By.Id("send2"));
+        private IWebElement _incorrectLogInMessageBox => Driver.FindElement(By.CssSelector(".message-error.error.message"));
+
+        public ParduotuvePegasasLogInPage(IWebDriver webdriver) : base(webdriver)
+        {
+            Driver.Url = PageAddress;
+        }
+        public ParduotuvePegasasLogInPage NavigateToDefaultPage()
+        {
+            if (Driver.Url != PageAddress)
+            {
+                Driver.Url = PageAddress;
+            }
+            return this;
+        }
+        public ParduotuvePegasasLogInPage LogInInfoInput(string email, string password)
+        {
+            _emailInputField.Click();
+            _emailInputField.SendKeys(email);
+            _passwordInputField.Click();
+            _passwordInputField.SendKeys(password);
+            return this;
+        }
+        public ParduotuvePegasasLogInPage LogInButtonClick()
+        {
+            _loginButton.Click();
+            return this;
+        }
+        public ParduotuvePegasasLogInPage VerifyIncorrectLoginErrorMessageDisplayed()
+        {
+            string errorMessage = "Prisijungimo duomenys neteisingi";
+            Assert.IsTrue(_incorrectLogInMessageBox.Text.Contains(errorMessage), $"{errorMessage} is not displayed, {_incorrectLogInMessageBox.Text} is displayed instead");
+            return this;
+        }
+    }
+}
---
+++ automatinis_testavimas/baig page/ParduotuvePegasasPegasoKolekcijosKnygosPage.cs
@@ -0,0 +1,45 @@
+﻿using automatinis_testavimas.page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatinis_testavimas.baig_page
+{
+    public class ParduotuvePegasasPegasoKolekcijosKnygosPage : BasePage
+    {
+        private const string PageAddress = "https://www.pegasas.lt/pegaso-kolekcijos-knygos.html";
+        //private const string ResultText = "";
+        //private IWebElement _bookName => Driver.FindElement(By.CssSelector(".product-item-name"));
+        private IReadOnlyCollection<IWebElement> _bookName => Driver.FindElements(By.CssSelector(".product-item-name"));
+        public ParduotuvePegasasPegasoKolekcijosKnygosPage(IWebDriver webdriver) : base(webdriver)
+        {       }
+        public ParduotuvePegasasPegasoKolekcijosKnygosPage NavigateToDefaultPage()
+        {
+            if (Driver.Url != PageAddress)
+            {
+                Driver.Url = PageAddress;
+            }
+            return this;
+        }
+        public ParduotuvePegasasPegasoKolekcijosKnygosPage VerifyBookIsDisplayed(string bookName)
+        {
+            Actions action = new Actions(Driver);
+
+            foreach (IWebElement book in _bookName)
+            {
+                IWebElement foundBook = null;
+                while (book.Text.ToUpper().Contains(bookName.ToUpper()))
+                {
+                    foundBook = book;
+                }
+                Assert.IsTrue(foundBook.Text.ToUpper().Contains(bookName.ToUpper()), $"Book is not displayed or the book title is {foundBook.Text} instead of {bookName}");
+            }
+            return this;
+        }
+    }
+}
